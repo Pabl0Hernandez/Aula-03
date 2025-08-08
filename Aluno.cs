74eac210817e12a7006dfaa2622dcd687518dee8
Sesi2 <Sesi2@SN-FWTB774>
2025-08-08 15:58:07 -0300

    Adicionar arquivos de projeto.

---
+++ Aluno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_03
+{
+    public class Aluno
+    {
+
+        public Aluno(int codigo, string nome)
+        {
+            this.Codigo = codigo;
+            this.Nome = nome;
+        }
+        public int Codigo { get; set; } // Atributos
+        public string? Nome { get; set; }
+    }
+}
